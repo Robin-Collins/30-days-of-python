1115928d46a07c857db20bb6efd5adb40fd14533
Robin Collins <84905211+Robin-Collins@users.noreply.github.com>
2022-08-28 18:29:15 +0930

    Day 1

---
+++ Day1/ExcerciseLevel2.py
@@ -0,0 +1,30 @@
+import sys
+
+#1 Check the python version you are using
+print("version = "+sys.version)
+
+#2 Do the following Operations. Operands 3 & 4
+print("3 + 4 = "+str(3+4))
+print("3 - 4 = "+str(3-4))
+print("3 x 4 = "+str(3*4))
+print("3 % 4 = "+str(3%4))
+print("3 / 4 = "+str(3/4))
+print("3 ** 4 = "+str(3**4))
+print("3 // 4 = "+str(3//4))
+
+#3 Write strings 
+print("Robin")
+print("Collins")
+print("Australia")
+print("I am enjoying 30 dyas of python")
+
+#4 Check data types
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh','Python', 'Finland']))
+print(type("Robin"))
+print(type("Collins"))
+print(type("Australia"))
+
---
+++ Day1/ExcerciseLevel3.py
@@ -0,0 +1,40 @@
+#Examples
+#Numbers
+
+
+vInteger=5
+vFloat=3.6
+vComplex=1 + 1j
+
+#String
+vString = "Hello World"
+print(type(vString))
+#Boolean
+vBoolean = False
+print(type(vBoolean))
+#List
+vList = ["30","days","of","python"]
+print(type(vList))
+#Tuple
+vTuple = ("10","20","100",40)
+print(type(vTuple))
+#Set
+vSet = {1,2,3,4,5}
+print(type(vSet))
+#Dictionary
+vDictionary={"first":"Robin", "surname":"collins","DOB":"20/02/1977"}
+print(type(vDictionary))
+
+# Euclidian Disance (2,3) (10,8)
+objA = (2,3)
+objB = (10,8)
+
+a = 10-2 # objB[0]-objA[0]
+b = 8-3  # objB[1]-objA[1]
+c = a**2
+d = b**2
+e = c+d
+f = e**.5
+
+print(f)
+
